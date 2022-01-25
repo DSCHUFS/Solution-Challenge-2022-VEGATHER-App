1a447896f4598cdc4508dfbab8d2519198a06afc
YEEUN <ykoou@naver.com>
2022-01-26 03:54:53 +0900

    Make Login Layout & Join Layout

---
+++ app/src/main/java/com/example/solution_challenge_2022_vegather_app/CustomEditText.kt
@@ -0,0 +1,100 @@
+package com.example.solution_challenge_2022_vegather_app
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.graphics.drawable.DrawableCompat
+import android.R
+import androidx.core.content.ContextCompat
+import android.graphics.drawable.Drawable
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.MotionEvent
+import android.view.View
+import android.view.View.OnFocusChangeListener
+import android.view.View.OnTouchListener
+
+//clear 버튼이 있는 EditText 위젯 
+class CustomEditText : AppCompatEditText, OnTouchListener, OnFocusChangeListener, TextWatcher {
+    private var clearDrawable: Drawable? = null
+    @get:JvmName("getOnFocusChangeListener()") private var onFocusChangeListener: OnFocusChangeListener? = null
+    private var onTouchListener: OnTouchListener? = null
+
+
+    constructor(context: Context) : super(context) { init() }
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) { init() }
+    constructor(context: Context, attrs:AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) { init() }
+
+    override fun setOnFocusChangeListener(onFocusChangeListener: OnFocusChangeListener?) {
+        this.onFocusChangeListener = onFocusChangeListener
+    }
+
+    override fun setOnTouchListener(onTouchListener: OnTouchListener?) {
+        this.onTouchListener = onTouchListener
+    }
+
+    fun init(){
+        val tempDrawable = ContextCompat.getDrawable(context, R.drawable.ic_menu_close_clear_cancel) //X 아이콘 추후 수정
+        clearDrawable = tempDrawable?.let { DrawableCompat.wrap(it) }
+        clearDrawable?.let { DrawableCompat.setTintList(it, hintTextColors) }
+        clearDrawable?.setBounds(
+            0,
+            0,
+            clearDrawable!!.getIntrinsicWidth(),
+            clearDrawable!!.getIntrinsicHeight()
+        )
+
+        setClearIconVisible(false)
+
+
+        super.setOnTouchListener(this)
+        super.setOnFocusChangeListener(this)
+        addTextChangedListener(this)
+    }
+
+    private fun setClearIconVisible(visible: Boolean) {
+        clearDrawable?.setVisible(visible, false)
+        if(visible){
+            setCompoundDrawables(null,null, clearDrawable, null);
+        }else{
+            setCompoundDrawables(null,null, null, null);
+        }
+    }
+
+    override fun onTouch(view: View?, motionEvent: MotionEvent?): Boolean {
+        if (motionEvent != null) {
+            if (clearDrawable!!.isVisible && motionEvent.getX() > width - paddingRight - clearDrawable!!.intrinsicWidth) {
+                if (motionEvent.getAction() === MotionEvent.ACTION_UP) {
+                    error = null
+                    setText(null)
+                }
+                return true
+            }
+        }
+
+        return onTouchListener?.onTouch(view, motionEvent) ?: false
+    }
+
+    override fun onFocusChange(view: View?, hasFocus: Boolean) {
+        if (hasFocus) {
+            setClearIconVisible(getText()?.length ?: 0 > 0);
+        } else {
+            setClearIconVisible(false);
+        }
+
+        onFocusChangeListener?.onFocusChange(view, hasFocus)
+    }
+
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+    }
+
+    override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+        if (isFocused) {
+            setClearIconVisible(s.length > 0)
+        }
+    }
+
+    override fun afterTextChanged(p0: Editable?) {
+    }
+
+}
---
+++ app/src/main/java/com/example/solution_challenge_2022_vegather_app/JoinActivity.kt
@@ -0,0 +1,11 @@
+package com.example.solution_challenge_2022_vegather_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JoinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join)
+    }
+}
