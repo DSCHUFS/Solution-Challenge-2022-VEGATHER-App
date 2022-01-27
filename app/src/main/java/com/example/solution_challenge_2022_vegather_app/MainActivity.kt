3e7c08090926cc3f255cb07aad81762dbafe9329
Yoon-Min <yoonmin0113@naver.com>
2022-01-27 15:29:13 +0900

    main commit

--- app/src/main/java/com/example/solution_challenge_2022_vegather_app/MainActivity.kt
+++ app/src/main/java/com/example/solution_challenge_2022_vegather_app/MainActivity.kt
@@ -8,7 +8,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        println("hello world!")
-        println("second line")
     }
 }
