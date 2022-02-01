0c0fffcb37ee53213d0cefcdce5626e8529c6a72
Yoon-Min <yoonmin0113@naver.com>
2022-02-01 16:39:02 +0900

    Add feature : switching activities
    - reduce the number of tap items
    - connect home activity with community activity

--- app/src/main/java/com/example/solution_challenge_2022_vegather_app/MainActivity.kt
+++ app/src/main/java/com/example/solution_challenge_2022_vegather_app/MainActivity.kt
@@ -1,19 +1,39 @@
 package com.example.solution_challenge_2022_vegather_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.solution_challenge_2022_vegather_app.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_page) //레이아웃 개발 동안 여길 변경해서 사용. .
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val intentCommunity = Intent(this, CommunityWriteActivity::class.java)
+        val intentMypage = Intent(this, MypageActivity::class.java)
 
-        print("Hello Yeeun")
-
-        println("hello world!")
-        println("second line")
-        println("yeeun world") //내 브랜치에서 작업한 코드
-        println("hahahhaa") //master에 올라와있던 최신 코드
+       binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+           // 탭 버튼을 선택할 때
+           override fun onTabSelected(tab: TabLayout.Tab?) {
+               switchActivity(tab)
+           }
+            // 다른 탭 버튼을 눌러 선택된 탭 버튼이 해제될 때 이벤트
+           override fun onTabUnselected(tab: TabLayout.Tab?) {
+           }
+            // 선택된 탭 버튼을 다시 선택할 때 이벤트
+           override fun onTabReselected(tab: TabLayout.Tab?) {
+                switchActivity(tab)
+           }
+            // 선택된 탭에 해당하는 액티비티로 화면 전환
+           fun switchActivity(tab: TabLayout.Tab?){
+               when(tab?.text){
+                   "Community" -> startActivity(intentCommunity)
+                   "Mypage" -> startActivity(intentMypage)
+               }
+           }
+       })
     }
 }
