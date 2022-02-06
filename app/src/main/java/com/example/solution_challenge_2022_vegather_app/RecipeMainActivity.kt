55cb7d9ca215498326f7e66f824f62366df44ed8
Yoon-Min <yoonmin0113@naver.com>
2022-02-07 02:36:47 +0900

    Add a new activity
    - provide detailed information on the recipe.
    - delete the temporarily designed xml file and move it to the RecipeMain activity.
    - create xml file to give dynamic effect.

--- app/src/main/java/com/example/solution_challenge_2022_vegather_app/RecipeMainActivity.kt
+++ app/src/main/java/com/example/solution_challenge_2022_vegather_app/RecipeMainActivity.kt
@@ -2,10 +2,32 @@ package com.example.solution_challenge_2022_vegather_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.solution_challenge_2022_vegather_app.databinding.ActivityRecipeMainBinding
+import com.example.solution_challenge_2022_vegather_app.databinding.IngredientRecyclerBinding
 
 class RecipeMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_recipe_main)
+        val binding = ActivityRecipeMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 'See More'를 통해 현재 액티비티에 접속했을 경우 값 업데이트 테스트코드
+        val intent = getIntent()
+        val foodName = intent.getStringExtra("foodName")
+        binding.textView27.text = foodName
+
+        binding.imageButton2.setOnClickListener {
+            finish()
+        }
+
+        binding.ingredientRecycler.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
+        val adapter = IngredientAdapter(IngredientRecyclerBinding.inflate(layoutInflater))
+        adapter.getData()
+        binding.ingredientRecycler.adapter = adapter
     }
 }
