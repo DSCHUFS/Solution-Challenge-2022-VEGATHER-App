689cdd51848e7d0f972c87385f146ea5b1b045ff
Yoon-Min <yoonmin0113@naver.com>
2022-02-11 22:03:33 +0900

    Add a feat - Add the order of the recipe. - The checked button is green and the unchecked button is gray.

--- app/src/main/java/com/example/solution_challenge_2022_vegather_app/RecipeMainActivity.kt
+++ app/src/main/java/com/example/solution_challenge_2022_vegather_app/RecipeMainActivity.kt
@@ -2,6 +2,7 @@ package com.example.solution_challenge_2022_vegather_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.LinearLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -9,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.solution_challenge_2022_vegather_app.databinding.ActivityRecipeMainBinding
 import com.example.solution_challenge_2022_vegather_app.databinding.IngredientRecyclerBinding
+import com.example.solution_challenge_2022_vegather_app.databinding.OrderRecyclerBinding
 
 class RecipeMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,10 +26,35 @@ class RecipeMainActivity : AppCompatActivity() {
         binding.imageButton2.setOnClickListener {
             finish()
         }
+        connectOrderAdapter(binding)
+        connectIngredientsAdapterWithOrientation("horizontal",binding)
 
-        binding.ingredientRecycler.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
+        binding.radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            Log.d("id",checkedId.toString())
+            when(checkedId) {
+                R.id.radioButton2 -> connectIngredientsAdapterWithOrientation("horizontal",binding)
+                R.id.radioButton3 -> connectIngredientsAdapterWithOrientation("vertical",binding)
+            }
+        }
+    }
+
+    private fun connectIngredientsAdapterWithOrientation(layout : String, binding : ActivityRecipeMainBinding){
+        when(layout){
+            "horizontal" -> binding.ingredientRecycler.layoutManager = LinearLayoutManager(
+                this, LinearLayoutManager.HORIZONTAL, false)
+
+            "vertical" -> binding.ingredientRecycler.layoutManager = LinearLayoutManager(
+                this)
+        }
         val adapter = IngredientAdapter(IngredientRecyclerBinding.inflate(layoutInflater))
         adapter.getData()
         binding.ingredientRecycler.adapter = adapter
+    }
+
+    private fun connectOrderAdapter(binding : ActivityRecipeMainBinding){
+        binding.orderRecycler.layoutManager = LinearLayoutManager(this)
+        val adapter = OrderAdapter(OrderRecyclerBinding.inflate(layoutInflater))
+        adapter.createTestData()
+        binding.orderRecycler.adapter = adapter
     }
 }
