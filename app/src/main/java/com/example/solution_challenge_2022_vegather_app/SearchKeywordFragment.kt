e64c3e795c7442cf212928ac5e9af6caa7181616
Yoon-Min <yoonmin0113@naver.com>
2022-02-20 21:10:08 +0900

    feat : Add automatic completion
    - Add to provide additional keywords
    - Adding text highlighting to indicate relevance

--- app/src/main/java/com/example/solution_challenge_2022_vegather_app/SearchKeywordFragment.kt
+++ app/src/main/java/com/example/solution_challenge_2022_vegather_app/SearchKeywordFragment.kt
@@ -1,25 +1,61 @@
 package com.example.solution_challenge_2022_vegather_app
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.solution_challenge_2022_vegather_app.databinding.FragmentSearchKeywordBinding
+import com.example.solution_challenge_2022_vegather_app.databinding.SearchAutocompleteRecyclerBinding
 
 class SearchKeywordFragment : Fragment() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        Log.d("text","호출")
         // Inflate the layout for this fragment
         val binding = FragmentSearchKeywordBinding.inflate(inflater,container,false)
-        binding.testText.text = "helloWorld!"
+        val bundle : Bundle? = arguments
+        val foodNameList = bundle?.getStringArrayList("foodNameList")
+        val startIndex = bundle?.getIntegerArrayList("startIndex")
+        val inputValueLength = bundle?.getInt("inputSearchLength")
+
+        binding.searchKeywordList.layoutManager = LinearLayoutManager(this.context)
+        val adapter = AutocompleteSearchAdapter(SearchAutocompleteRecyclerBinding.inflate(layoutInflater))
+
+        if( foodNameList!=null && startIndex!=null && inputValueLength!=null ){
+            adapter.setData(foodNameList,startIndex,inputValueLength)
+        }
+        binding.searchKeywordList.adapter = adapter
+
+        binding.searchKeywordList.setOnTouchListener { v, event ->
+            when(event.action){
+                MotionEvent.ACTION_DOWN -> {
+                    hideKeyBoard()
+                }
+            }
+            false
+        }
+
         return binding.root
     }
-
+    private fun hideKeyBoard() {
+        val imm = requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(requireActivity().currentFocus?.windowToken, 0)
+    }
 }
+
--- app/src/main/java/com/example/solution_challenge_2022_vegather_app/SearchRankingAndHistoryFragment.kt
+++ app/src/main/java/com/example/solution_challenge_2022_vegather_app/SearchRankingAndHistoryFragment.kt
@@ -1,12 +1,17 @@
 package com.example.solution_challenge_2022_vegather_app
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.LinearLayout
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.solution_challenge_2022_vegather_app.databinding.FragmentSearchRankingAndHistoryBinding
@@ -24,7 +29,7 @@ class SearchRankingAndHistoryFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         val binding = FragmentSearchRankingAndHistoryBinding.inflate(inflater,container,false)
 
@@ -36,9 +41,11 @@ class SearchRankingAndHistoryFragment : Fragment() {
         binding.allClearButton.setOnClickListener {
             recycler.deleteAllSearchHistory()
         }
-
         return binding.root
+
     }
+
+
 
 }
 
