dab7c2e08f1fc7e0ef2a9c9231508156a5e57ba8
김준욱 <wqsaxz6996@gmail.com>
2022-03-21 03:30:18 +0900

    - commit before merge

--- app/src/main/java/com/example/solution_challenge_2022_vegather_app/CommunityDetailActivity.kt
+++ app/src/main/java/com/example/solution_challenge_2022_vegather_app/CommunityDetailActivity.kt
@@ -10,6 +10,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import android.widget.ImageView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
